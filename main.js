5fa9d91a808315d05c0d2ac876159ff83c1a9b85
Blake <blake.doyle@hotmail.com.au>
2018-11-25 17:44:45 +1100

    initialise and add displaytime function

---
+++ main.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    function displayTime() {
+        let currentTime = new Date();
+        let hours = currentTime.getHours();
+        let minutes = currentTime.getMinutes();
+        let seconds = currentTime.getSeconds();
+    }
+});
