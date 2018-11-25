d0eb7ea7325c87132e05aaa6422cb42f7ff68715
Blake <blake.doyle@hotmail.com.au>
2018-11-25 18:18:49 +1100

    add 0 to seconds, allow for AM and PM

--- main.js
+++ main.js
@@ -4,5 +4,24 @@ $(document).ready(function() {
         let hours = currentTime.getHours();
         let minutes = currentTime.getMinutes();
         let seconds = currentTime.getSeconds();
+        let timeState = "AM";
+
+        let clockDiv = document.getElementById('clock');
+
+        if (seconds < 10){
+            seconds = "0" + seconds;
+        }
+
+        if (hours > 12) {
+            hours = hours - 12;
+            timeState = "PM";
+        }
+
+        if (hours === 0){
+            hours = 12;
+        }
+        
+        clockDiv.innerText = hours + ":" + minutes + ":" + seconds + " " + timeState;
     }
+    setInterval(displayTime,1000);
 });
